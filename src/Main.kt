e5adde81e12920e7e466035266e88e7d4c346291
Yaar-Nuul <yaarnuulreech@gmail.com>
2024-05-03 09:10:32 +0300

    add changes

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,47 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val store = ItemAndCategory()
+//
+    store.addPreferences("oxford")
+    val preferredItem = "oxford"
+    store.recommendations(preferredItem)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+}
+
+
+//Desing a recommendation system for an e-commerce platform that allows fast retrieval
+//of recommendations for a given user. Each user has a set of preferred items, and you
+//need to efficiently retrieve recommendations based on their preferences.
+
+
+class ItemAndCategory{
+
+    var shoes = mutableListOf("boot",  "oxford", "clark", "sandal", "clog")
+    var jewelry= mutableListOf("watch", "earrings", "necklaces", "rings", "nose-rings")
+    var clothes = mutableListOf("sweater", "trouser", "jots", "jacket", "hoodie")
+
+
+
+
+
+var userPreferences = mutableListOf<String>()
+fun addPreferences(item:String){
+    userPreferences.add(item)
+}
+fun recommendations(item: String){
+    if (item in shoes){
+        println(shoes)
     }
+    else if (item in jewelry){
+        println(jewelry)
+    }
+    else if (item in clothes){
+        println(clothes)
+    }
+    else{
+        println("Item not found")
+    }
+}
 }
